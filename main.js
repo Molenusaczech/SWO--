21c9a5c6a687f865b5d13ac6a247b3cb0a2675f4
Molenusaczech <06krtek@gmail.com>
2022-10-30 15:16:57 +0100

    implement login system

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
 console.log("SWO++: Starting injection");
 //document.body.style.backgroundColor = 'orange';
 /*
@@ -42,3 +50,83 @@ chrome.storage.sync.get(['theme'], function(result) {
     }
   });
 
+chrome.storage.sync.get(['loginStatus'], function(result) {
+    var link = window.location.href;
+    var token = "placeholder";
+
+    chrome.storage.sync.get(['token'], function(result) {
+      token = result.token;
+    });
+
+    //console.log('SWO++: Login status currently is ' + result.loginStatus);
+    if (result.loginStatus == "1" && link == "https://scratchwars-online.cz/cs/account/") {
+      console.log("SWO++: LogStatus is 1, fetching id");
+      var id = document.querySelector(".user-id").innerHTML;
+      id = id.replace("#", "");
+      console.log("SWO++: id: "+id);
+      
+      chrome.storage.sync.set({"login": id}, function() {
+        console.log("SWO++: Login set to: "+id);
+
+        chrome.storage.sync.set({"loginStatus": "2"}, function() {
+          window.location.href = "https://scratchwars-online.cz/cs/account/collection/"+id;
+        });
+
+      });
+    } else if (result.loginStatus == "2") {
+      
+      chrome.storage.sync.get(['login'], function(result) {
+        var login = result.login;
+
+        if (link == "https://scratchwars-online.cz/cs/account/collection/"+login) {
+          console.log("SWO++: LogStatus is 2, fetching collection");
+
+          //document.querySelector('[data-hero-family-id="68"]').click();
+          //document.querySelectorAll('[data-hero-family-id="68"]')[1].click();
+
+          // implementovat mladého dobrodruha - 103
+          var iid = document.querySelectorAll('[data-hero-family-id="68"]')[1].children[0].dataset.iid;
+          
+          
+          chrome.storage.sync.set({"verifyiid": iid}, function() {
+
+            chrome.storage.sync.get(['pin'], function(result) {
+              var pin = result.pin;
+
+              //var response = fetch("https://SWOPPServer.mole06.repl.co/index.php?token="+token+"&iid="+iid+"&login="+login+"&pin="+pin+"&card=saman").then(window.location.href = chrome.runtime.getURL("settings.html"));
+
+              var url = "https://SWOPPServer.mole06.repl.co/index.php?token="+token+"&iid="+iid+"&login="+login+"&pin="+pin+"&card=saman";
+      
+              var xhr = new XMLHttpRequest();
+              xhr.open("GET", url);
+              
+              xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                  console.log(xhr.status);
+                  console.log(xhr.responseText);
+                  chrome.storage.sync.set({"loginStatus": "3"}, function() {
+                    window.location.href = chrome.runtime.getURL("settings.html");
+                  });    
+                  
+              }};
+        
+              xhr.send();
+
+              
+
+              /*
+              chrome.storage.sync.set({"loginStatus": "3"}, function() {
+                window.location.href = chrome.runtime.getURL("settings.html");
+              });*/
+
+            });
+
+            
+
+            
+          });
+          
+        }
+      });
+  }
+});
--- setjs.js
+++ setjs.js
@@ -6,7 +6,32 @@ function changeTheme() {
       });
 }
 
+function login() {
+
+  var token = makeid(32);
+  var pin = document.getElementById("pin").value;
+
+  chrome.storage.sync.set({"token": token});
+  chrome.storage.sync.set({"pin": pin});
+
+  chrome.storage.sync.set({"loginStatus": "1"}, function() {
+    window.location.href = "https://scratchwars-online.cz/cs/account/";
+  });
+
+}
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 document.getElementById("theme").addEventListener("change", changeTheme);
+document.getElementById("login").addEventListener("click", login);
 
 console.log("SWO++: Starting injection");
 
@@ -14,5 +39,42 @@ chrome.storage.sync.get(['theme'], function(result) {
     console.log('Theme currently is ' + result.theme);
     document.getElementById("theme").value = result.theme;
   });
+
+
+chrome.storage.sync.get(['login'], function(result) {
+    console.log('Login currently is ' + result.login);
+    var login = result.login;
+    document.getElementById("logintext").innerHTML = "Logged in as: "+ result.login;
+
+    chrome.storage.sync.get(['token'], function(result) {
+      console.log('Token currently is ' + result.token);
+      var token = result.token;
+      var url = "https://swoppserver.mole06.repl.co/check.php?id="+login+"&token="+token;
+      
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          console.log(xhr.status);
+          console.log(xhr.responseText);
+          
+          if (xhr.responseText == "valid") {
+            document.getElementById("loginstatus").innerHTML = "Tvoje přihlášení je platné.";
+          } else {
+            document.getElementById("loginstatus").innerHTML = "Tvoje přihlášení není platné.";
+          }
+          
+      }};
+
+      xhr.send();
+      
+   
+
+    });
+    
+
+});
+
 
 
